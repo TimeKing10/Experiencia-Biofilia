7eee2ef1d0d9a8bf86fdea33914eea5b758b8e83
TimeKing <kilaganyero@gmail.com>
2025-05-22 17:09:35 -0500

    Actualizacion sistema fotos resumen

---
+++ Assets/Scripts/AndresVelez/MecaniacasJuego/FotoAnimal.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEditor;
+[System.Serializable]
+
+public class FotoAnimal
+{
+    public string nombreAnimal;
+    public Texture2D foto;
+    public int score;
+}
